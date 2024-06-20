0883ad4ea2c834632d262119bb0fa9d110c73140
Afif Roslan <afifroslan95@gmail.com>
2024-06-20 13:59:21 +0800

    Added function for rock paper scissors game

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let choice = Math.random() * 10;
     if (choice <= 3) {
@@ -14,3 +17,31 @@ function getHumanChoice() {
     choice = choice.toLowerCase();
     return choice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    let humanWin = `You win! ${humanChoice} beats ${computerChoice}`;
+    let computerWin = `You lose! ${computerChoice} beats ${humanChoice}`;
+    if (humanChoice == computerChoice) {
+        return "It's a draw";
+    } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        humanScore += 1;
+        return humanWin;
+    } else if (humanChoice == "rock" && computerChoice == "paper") {
+        computerScore += 1;
+        return computerWin;
+    } else if (humanChoice == "scissor" && computerChoice == "rock") {
+        computerScore += 1;
+        return computerWin;
+    } else if (humanChoice == "scissor" && computerChoice == "paper") {
+        humanScore += 1;
+        return humanWin;
+    } else if (humanChoice == "paper" && computerChoice == "scissor") {
+        computerScore += 1;
+        return computerWin;
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        humanScore += 1;
+        return humanWin;
+    }
+}
+
+console.log(playRound(getHumanChoice(), getComputerChoice()));
