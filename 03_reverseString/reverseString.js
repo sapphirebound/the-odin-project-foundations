6ad76e8897f936de15810233707227dee98c85fa
Afif Roslan <afifroslan95@gmail.com>
2024-06-21 18:27:34 +0800

    completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let originalText = String(string);
+    let Ltext = originalText.length;
+    let reversedText = '';
+    for (let i = 1; i <= Ltext; i++) {
+        let key = -i;
+        let letter = originalText.at(key);
+        reversedText += letter;
+    }
+    return reversedText;
 };
 
 // Do not edit below this line
