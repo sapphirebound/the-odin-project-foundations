12a6f1f9dfe3f2223dd1e84323596680e4514657
Afif Roslan <afifroslan95@gmail.com>
2024-06-21 18:18:56 +0800

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+    if (number < 0) {
+        return 'ERROR';
+    } else {
+        let text = '';
+        console.log(toString(string));
+        for (let i = 0; i < number; i++) {
+            text += string;
+        }
+        return text;
+    }
 };
+
+repeatString('hey', 3);
+repeatString('hello', 10);
+repeatString('hi', 1);
+repeatString('bye', 0);
+repeatString('goodbye', -1);
+const number = require('./repeatString.spec');
+repeatString('odin', number);
+repeatString('', 10);
+
 
 // Do not edit below this line
 module.exports = repeatString;
