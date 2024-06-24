ddca325b8897b7c5746ed97825516ae60d6e7a93
Afif Roslan <afifroslan95@gmail.com>
2024-06-24 17:58:59 +0800

    finished exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function (a, b) {
+  let result = 0;
+  result = a + b;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  let result = 0;
+  result = a - b;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  let result = 0;
+  for (num of numbers) result += num;
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  let result = 0;
+  result = numbers[0];
+  numbersLength = numbers.length;
+  for (let num = 1; num < numbersLength; num++) {
+    result = result * numbers[num];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function (number, power) {
+  let result = 0;
+  result = number ** power;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let result = 1;
+  let factorialLength = Array(number).length;
+  for (i = 1; i <= factorialLength; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
