1b6b7515455010a149f605584f5117cda4a1addc
Afif Roslan <afifroslan95@gmail.com>
2024-06-24 17:16:29 +0800

    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+let result = 0;
+
+const convertToCelsius = function (num) {
+  result = (num - 32) * 5 / 9;
+  result = Number(result.toFixed(1));
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+  result = (num * 9 / 5) + 32;
+  result = Number(result.toFixed(1));
+  return result;
 };
 
 // Do not edit below this line
