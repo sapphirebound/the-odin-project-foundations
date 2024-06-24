246b713be3dd3f8abcff20df64a0077570bbfe4b
Afif Roslan <afifroslan95@gmail.com>
2024-06-24 18:33:14 +0800

    completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    word = word.toLowerCase(); //make everything lowercase
+    word = word.replaceAll(/\W/gi, ""); //remove non-letter
+    word = word.trim(); //trim white space
+    reversedWord = ""; // set variable for reversed word
 
+    //iterating over each letter in word in reverse
+    for (let i = 1; i <= word.length; i++) {
+        let letter = word.at(-i);
+        reversedWord += letter;
+    }
+    return word === reversedWord;
 };
 
 // Do not edit below this line
