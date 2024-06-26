8888b1c1d8b3256eba3f1d1e5c90d694247169c2
Afif Roslan <afifroslan95@gmail.com>
2024-06-26 17:43:58 +0800

    Added buttons and assigned event listeners to return human choice

--- javascript.js
+++ javascript.js
@@ -1,3 +1,25 @@
+const menu = document.querySelector(".menu");
+let humanChoice = '';
+
+menu.addEventListener("click", (event) => {
+    let target = event.target;
+
+    switch (target.id) {
+        case 'rock':
+            console.log('rock');
+            humanChoice = 'rock';
+            break;
+        case 'scissors':
+            console.log('scissors');
+            humanChoice = 'scissors';
+            break;
+        case 'paper':
+            console.log('paper');
+            humanChoice = 'paper';
+            break;
+    }
+});
+
 function getComputerChoice() {
     let choice = Math.random() * 10;
     if (choice <= 3) {
@@ -15,38 +37,40 @@ function getHumanChoice() {
     return choice;
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    function playRound(humanChoice, computerChoice) {
-        let humanWin = `You win! ${humanChoice} beats ${computerChoice}`;
-        let computerWin = `You lose! ${computerChoice} beats ${humanChoice}`;
-        if (humanChoice == computerChoice) {
-            console.log("It's a draw");
-        } else if (humanChoice == "rock" && computerChoice == "scissors") {
-            humanScore += 1;
-            console.log(humanWin);
-        } else if (humanChoice == "rock" && computerChoice == "paper") {
-            computerScore += 1;
-            console.log(computerWin);
-        } else if (humanChoice == "scissors" && computerChoice == "rock") {
-            computerScore += 1;
-            console.log(computerWin);
-        } else if (humanChoice == "scissors" && computerChoice == "paper") {
-            humanScore += 1;
-            console.log(humanWin);
-        } else if (humanChoice == "paper" && computerChoice == "scissors") {
-            computerScore += 1;
-            console.log(computerWin);
-        } else if (humanChoice == "paper" && computerChoice == "rock") {
-            humanScore += 1;
-            console.log(humanWin);
-        }
-    }
-    while (humanScore < 5 || computerScore < 5) {
-        playRound(getHumanChoice(), getComputerChoice());
-        console.log(`com: ${computerScore} hum: ${humanScore}`);
-    }
-}
+menu.onclick;
 
-playGame();
+// function playGame() {
+//     let humanScore = 0;
+//     let computerScore = 0;
+//     function playRound(humanChoice, computerChoice) {
+//         let humanWin = `You win! ${humanChoice} beats ${computerChoice}`;
+//         let computerWin = `You lose! ${computerChoice} beats ${humanChoice}`;
+//         if (humanChoice == computerChoice) {
+//             console.log("It's a draw");
+//         } else if (humanChoice == "rock" && computerChoice == "scissors") {
+//             humanScore += 1;
+//             console.log(humanWin);
+//         } else if (humanChoice == "rock" && computerChoice == "paper") {
+//             computerScore += 1;
+//             console.log(computerWin);
+//         } else if (humanChoice == "scissors" && computerChoice == "rock") {
+//             computerScore += 1;
+//             console.log(computerWin);
+//         } else if (humanChoice == "scissors" && computerChoice == "paper") {
+//             humanScore += 1;
+//             console.log(humanWin);
+//         } else if (humanChoice == "paper" && computerChoice == "scissors") {
+//             computerScore += 1;
+//             console.log(computerWin);
+//         } else if (humanChoice == "paper" && computerChoice == "rock") {
+//             humanScore += 1;
+//             console.log(humanWin);
+//         }
+//     }
+//     while (humanScore < 5 || computerScore < 5) {
+//         playRound(getHumanChoice(), getComputerChoice());
+//         console.log(`com: ${computerScore} hum: ${humanScore}`);
+//     }
+// }
+
+// playGame();
