d0676b630e6bde2e8c378a7ad56bcd100b69a74e
Afif Roslan <afifroslan95@gmail.com>
2024-06-27 13:40:56 +0800

    changed input box to prompt and added opacity logic

--- javascript.js
+++ javascript.js
@@ -3,31 +3,16 @@ const content = document.querySelector(".content");
 //create header div for user input
 const header = document.createElement("div");
 header.className = "header";
+content.appendChild(header);
 
-//create input field
-const inputBox = document.createElement("input");
-inputBox.type = "number";
-inputBox.max = 100;
-inputBox.min = 0;
+//create input button
 const inputButton = document.createElement("button");
-inputButton.textContent = 'submit';
-
-header.appendChild(inputBox);
+inputButton.textContent = 'Choose Grid Size';
 header.appendChild(inputButton);
 
-//Create grid div for etch-a-sketch
-const grid = document.createElement("div");
-grid.className = "grid";
 
-let gridSize = 16;
-
-
-content.appendChild(header);
-content.appendChild(grid);
-
+//function to draw grid
 let count = 0;
-
-
 function drawGrid(gridSize) {
     grid.innerHTML = '';
     let boxWidth = (100 / gridSize) + '%';
@@ -37,28 +22,62 @@ function drawGrid(gridSize) {
         box.className = "box";
         box.id = count;
         box.style.width = boxWidth;
+        box.style.opacity = 0;
         grid.appendChild(box);
         count += 1;
     }
 }
 
+//Create grid div for etch-a-sketch
+const grid = document.createElement("div");
+grid.className = "grid";
+content.appendChild(grid);
+
+//draw default 16x16 grid
 drawGrid(16);
 
+//random color function
+function rand(min, max) {
+    return min + Math.random() * (max - min);
+}
+
+function getRandomColor() {
+    let h = rand(1, 360);
+    let s = rand(0, 100);
+    let l = rand(0, 100);
+    return 'hsl(' + h + ',' + s + '%,' + l + '%)';
+}
+
+//function to add opacity to target div
+function addOpacity(target) {
+    let opacity = Number(target.style.opacity);
+    if (opacity < 1) {
+        console.log('true');
+        opacity += 0.1;
+    } else { opacity = 1; };
+    return opacity;
+}
+
+//mouseover event to change color of box
 content.addEventListener("mouseover", (event) => {
     let target = event.target;
     if (target.className === "box") {
-        target.style.backgroundColor = "red";
+        target.style.opacity = addOpacity(target); //add 0.1 opacity
+        if (target.style.opacity == 1) {
+            target.style.backgroundColor = "black"; //color black if full opacity
+        } else target.style.backgroundColor = getRandomColor();
+
     }
 }
 );
 
-// Get input for grid creator
+//prompt user and get input for grid creator
 inputButton.addEventListener("click", () => {
-    let input = Number(inputBox.value);
+    let input = prompt("Choose Grid Size", 16);
     if (input <= 100) {
         gridSize = input;
-        inputBox.value = 16;
-        drawGrid(gridSize);
-    }
+    } else gridSize = 100;
+    inputBox.value = 16;
+    drawGrid(gridSize);
 }
 );
