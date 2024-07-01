81857f1331f7576630fbbca232625ecd599b401b
Afif Roslan <afifroslan95@gmail.com>
2024-07-01 16:21:43 +0800

    Initialise HTML and created basic functions

---
+++ calculator/calculator.js
@@ -0,0 +1,23 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+let firstNumber = 0;
+let operator = '';
+let secondNumber = 0;
+let display = 0;
+
+const operate = function (firstNumber, operator, secondNumber) {
+    switch (operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return divide(firstNumber, secondNumber);
+    }
+};
+
