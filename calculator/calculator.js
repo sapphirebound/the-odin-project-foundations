1d171be63d3c1629921d65a1da427efb61692189
Afif Roslan <afifroslan95@gmail.com>
2024-07-01 16:44:49 +0800

    added styles and basic calculator buttons

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,3 +21,38 @@ const operate = function (firstNumber, operator, secondNumber) {
     }
 };
 
+//numbers component
+const numberPad = document.createElement("div");
+numberPad.className = 'number-pad';
+
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let operators = ['+', '-', '*', '/'];
+
+for (operator in operators) {
+    const numberKey = document.createElement("btn");
+    const operatorKey = operators[operator];
+    numberKey.className = 'number-pad key';
+    numberKey.id = operatorKey;
+    numberKey.textContent = operatorKey;
+    numberPad.appendChild(numberKey);
+}
+
+for (num in numbers) {
+    const numberKey = document.createElement("btn");
+    numberKey.className = 'number-pad key';
+    numberKey.id = num;
+    numberKey.textContent = num;
+    numberPad.appendChild(numberKey);
+}
+
+//result screen
+const displayScreen = document.createElement("div");
+displayScreen.className = 'display-screen';
+displayScreen.style.backgroundColor = '#D9EEE1';
+displayScreen.style.display = 'flex';
+displayScreen.textContent = display;
+
+document.querySelector("body").appendChild(displayScreen);
+document.querySelector("body").appendChild(numberPad)
+
